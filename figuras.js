01b8f808a67193a6a9deba8f960ddbffde95ab90
AbdiDaniel <abdid81@gmail.com>
2021-12-28 23:28:21 -0500

    Más ejercicios

--- figuras.js
+++ figuras.js
@@ -94,3 +94,26 @@ function onClickDescuentoProducto(){
 
 }
 
+///////////////////////////////////////////////////////////////////////////////////////////////
+//Promedio, moda, mediana
+
+
+
+function calcularPromedioAritmetico(lista){
+
+// let sumaLista = 0;
+// for (let i = 0; i < lista.length; i++){
+//     sumaLista = sumaLista + lista[i];
+// }
+
+const sumaLista = lista.reduce(
+    function (valorAcumulado = 0, nuevoElemento ){
+        return valorAcumulado + nuevoElemento;
+    }
+);
+
+const promedioLista = sumaLista / lista.length;
+
+return promedioLista;
+
+}
